bc0ce1f083978aacc53d080c20598866d6027a36
Gilson Urbano <guf@hotmail.com.br>
2023-07-12 18:55:12 +0000

    Add command line using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Command;
+use std::io;
+
+fn cmd() -> clap::Command {
+    Command::new("lineselect")
+        .version("0.1.0")
+        .author("Gilson Urbano <me@gilsonurbano.com>")
+        .about("Select lines")
+}
+
+#[test]
+fn verify_cmd() {
+    cmd().debug_assert();
+}
+
 fn main() {
-    println!("Line Select!");
+    cmd().get_matches();
+
+    for line in io::stdin().lines() {
+        println!("{}", line.unwrap());
+    }
 }
